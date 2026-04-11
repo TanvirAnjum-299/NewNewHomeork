9b308510b08b933d50d98732115c4f312c6552b2
TanvirAnjum-299 <sinthiaibnat79@gmail.com>
2026-04-11 13:40:10 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+studentname="robert"#string
+age="15"#string
+Which_Grade="6"#string
+print("Name:", studentname)
+print("Data Type of Name is:", type(studentname))
+print("After Type Casting...")
+Which_Grade=str(Which_Grade)
+print(Which_Grade)
+print("Data Type of Which_Grade is:", type(Which_Grade))
