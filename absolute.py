944208069e17f159d6b9bf1393760858be7b5aaf
TanvirAnjum-299 <sinthiaibnat79@gmail.com>
2026-04-26 09:08:39 +0600

    Add files via upload

---
+++ absolute.py
@@ -0,0 +1,7 @@
+def absolute_value(number):
+    if number>=0:
+        return number
+    else:
+        return -number
+print("Absolute Value Of 89:",absolute_value(89))
+print("Absolute Value Of -189:",absolute_value(-189))
